ba414184664cbea3b35d1d86d794c9ab350eeb4b
unknown <you@example.com>
2022-11-18 14:44:39 -0300

    api feita com sucesso

--- script.js
+++ script.js
@@ -5,34 +5,30 @@ var button = document.querySelector(".btn")
 button.onclick = buscarCEP
 
 function buscarCEP(){
+    
     let cep = input.value
-    input.value = ""
     verificarCEP(cep)
-
-    let tags = ["tr", "td", "td", "td", "td", "td", "td"]
-    listaTag = []
-    listaTag = criarTags(tags, listaTag)
+    
 }
 
 function verificarCEP(cep, listaTag){
+
     let url = `https://viacep.com.br/ws/${cep}/json/`
            
     fetch(url)
         .then(response => {
             return response.json()
         })
-        .then(response => {
 
-            listaTag[1].textContent = response.cep
-            listaTag[2].textContent = response.logradouro
-            listaTag[3].textContent = response.complemento
-            listaTag[4].textContent = response.bairro
-            listaTag[5].textContent = response.localidade
-            listaTag[6].textContent = response.uf
-            listaTag[7].textContent = response.ddd
-            
-            console.log(response)
+        .then(response => {
+            let tags = ["tr", "td", "td", "td", "td", "td", "td", "td"]
+            let listaTag = []
+
+            listaTag = criarTags(tags, listaTag)
+            preencherTDs(listaTag, response)
+            inserirNaTela(listaTag, response)
         })
+
 }
 
 function criarTags(tags, listaTag) {
@@ -42,4 +38,30 @@ function criarTags(tags, listaTag) {
     });
 
     return listaTag
+}
+
+function preencherTDs(listaTag, response) {
+
+    listaTag[1].textContent = response.cep
+    listaTag[2].textContent = response.logradouro
+    listaTag[3].textContent = response.complemento
+    listaTag[4].textContent = response.bairro
+    listaTag[5].textContent = response.localidade
+    listaTag[6].textContent = response.uf
+    listaTag[7].textContent = response.ddd
+    
+}
+
+function inserirNaTela(listaTag) {
+    let body = document.querySelector('.corpoTabela')
+
+    body.appendChild(listaTag[0])
+
+    for (let contador = 1; contador < listaTag.length; contador++){
+
+        listaTag[0].appendChild(listaTag[contador])
+
+    }
+        
+    
 }
